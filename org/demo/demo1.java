d1bf954ce382e4f05081292c4ab7eb2a9ce11f16
AbarajithaMurugan <abarajitha1995murugan@gmail.com>
2021-03-01 10:36:38 +0530

    b2

--- org/demo/demo1.java
+++ org/demo/demo1.java
@@ -2,7 +2,7 @@ package org.demo;
 
 public class demo1 {
 	public static void main(String[] args) {
-		System.out.println("21234567");
+		System.out.println("asdfghjk");
 	}
 
 }
--- org/demo/demo2.java
+++
@@ -1,8 +0,0 @@
-package org.demo;
-
-public class demo2 {
-	public static void main(String[] args) {
-		System.out.println("asdfghj");
-	}
-
-}
